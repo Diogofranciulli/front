1489b7d0e32124311be02255fa33e6c2c511ae1c
Diogo Leles Franciulli <54507748+Diogofranciulli@users.noreply.github.com>
2024-09-25 22:46:17 -0300

    juntando back e front

--- aula12 - gemini-bootstrap/gemini-back/teste.js
+++ aula12 - gemini-bootstrap/gemini-back/teste.js
@@ -1,9 +1,9 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("API KEY");
+const genAI = new GoogleGenerativeAI("AIzaSyBkBFKVHGuveLM1xBuqBEHFRKSKJuzJ0ec");
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "acabei de terminar meu namoro, oq eu faço para superar??";
+const prompt = "quem esta ganhando as pesquisas eleitorais, sao paulo 2024??";
 
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
